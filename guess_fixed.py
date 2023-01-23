f7e9462c6563eb74bdb28ae91f6542ee11e08067
nstonic <nstonic@gmail.com>
2023-01-23 17:46:05 +0400

    Add argparse and fix types

--- guess_fixed.py
+++ guess_fixed.py
@@ -11,14 +11,14 @@ def get_rand_word(filename: str) -> str:
         return random.choice([line.strip() for line in file]).lower()
 
 
-def print_turn(target_word: str, guessed_letters: list[str], wrong_guesses: list):
+def print_turn(target_word: str, guessed_letters: list, wrong_guesses: list):
     print('\nWrong guess:', ', '.join(wrong_guesses))
     print(' '.join([letter if letter else ' ' for letter in guessed_letters]))
     masked_word = [character if character == ' ' else '-' for character in target_word]
     print(' '.join(masked_word))
 
 
-def get_letter(attempts: int, previous_guesses: list[str]) -> str:
+def get_letter(attempts: int, previous_guesses: list) -> str:
     user_guess = input(f'> Guess a letter ({attempts} attempts left): ').lower()
     if len(user_guess) != 1 or user_guess not in ascii_lowercase:
         print("> Please input a letter!")
@@ -28,8 +28,7 @@ def get_letter(attempts: int, previous_guesses: list[str]) -> str:
         return user_guess
 
 
-def main(target_word: str):
-    attempts = 6
+def play_the_game(target_word: str, attempts: int):
     previous_guesses = []
     wrong_guesses = []
     guessed_letters = [letter if letter == ' ' else None for letter in target_word]
@@ -40,14 +39,13 @@ def main(target_word: str):
             break
         user_letter = get_letter(attempts, previous_guesses)
         if user_letter is not None:
+            for index, letter in enumerate(target_word):
+                if user_letter == letter:
+                    guessed_letters[index] = user_letter
+            previous_guesses.append(user_letter)
             if user_letter not in guessed_letters:
                 wrong_guesses.append(user_letter)
                 attempts -= 1
-            else:
-                for index, letter in enumerate(target_word):
-                    if user_letter == letter:
-                        guessed_letters[index] = user_letter
-                previous_guesses.append(user_letter)
 
     else:
         print_turn(target_word, guessed_letters, wrong_guesses)
@@ -55,11 +53,20 @@ def main(target_word: str):
         print(f'> Answer: {target_word.capitalize()}')
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--sowpods',
                         default='sowpods.txt',
                         help='The name of the word file located in the sowpods folder')
+    parser.add_argument('--attempts',
+                        type=int,
+                        default=6,
+                        help='The number of attempts')
     args = parser.parse_args()
-    target_word = get_rand_word(os.path.join('sowpods', args.filename))
-    main(target_word)
+    file_path = os.path.join('sowpods', args.sowpods)
+    target_word = get_rand_word(file_path)
+    play_the_game(target_word, args.attempts)
+
+
+if __name__ == "__main__":
+    main()
